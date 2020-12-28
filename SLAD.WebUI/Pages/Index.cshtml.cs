6ab2c1fc19f3745e5e91d1ad5fed4ce0563c8a59
Jacob Westall <jlwdrums94@gmail.com>
2020-12-27 22:17:07 -0600

    Updated index page look and added item sorting

--- SLAD.WebUI/Pages/Index.cshtml.cs
+++ SLAD.WebUI/Pages/Index.cshtml.cs
@@ -20,15 +20,29 @@ namespace SLAD.WebUI.Pages
             ListItems = await DataAccess.LoadData<ListItem, dynamic>("select * from listitems", new { }, @"Data Source=Data/shoppinglist.db;Version=3;");
         }
 
-        public async Task OnBeforeUnloadAsync()
+        public async Task<IActionResult> OnPostMark(int id)
+        {
+            await DataAccess.SaveData<dynamic>($"update listitems set IsSelected = 1 where ID = { id }", new { }, @"Data Source=Data/shoppinglist.db;Version=3;");
+            return RedirectToPage("/Index");
+        }
+
+        public async Task<IActionResult> OnPostUnmark(int id)
+        {
+            await DataAccess.SaveData<dynamic>($"update listitems set IsSelected = 0 where ID = { id }", new { }, @"Data Source=Data/shoppinglist.db;Version=3;");
+            return RedirectToPage("/Index");
+        }
+
+        public async Task<IActionResult> OnPostDelete()
         {
             foreach (ListItem item in ListItems)
             {
                 if (item.IsSelected)
                 {
-                    await DataAccess.SaveData<ListItem>("update listitems set IsSelected = 1 where ItemName = @ItemName", item, @"Data Source=Data/shoppinglist.db;Version=3;");
+                    await DataAccess.SaveData<ListItem>("delete from listitems where id = @ID", item, @"Data Source=Data/shoppinglist.db;Version=3;");
                 }
             }
+
+            return RedirectToPage("/Index");
         }
     }
 }
